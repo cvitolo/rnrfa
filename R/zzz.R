c788ea6cc887238199409b07a3ca2797e17fed12
Claudia Vitolo <claudia.vitolo@gmail.com>
2016-12-23 18:53:00 +0000

    Added website to DESCRITPION

--- R/zzz.R
+++ R/zzz.R
@@ -5,22 +5,20 @@
 # spill-over: output information about RNRFA
 .onAttach <- function(...) {
 
-  package.name <- "rnrfa"
-  mylib <- dirname(system.file(package = package.name))
+  # package.name <- "rnrfa"
+  # mylib <- dirname(system.file(package = package.name))
   # ver <- utils::packageDescription(package.name, lib.loc = mylib)$Version
   #build.date <- utils::packageDescription(package.name, lib.loc = mylib)$Date
 
-
   # build info
   # packageStartupMessage("RNRFA (Versions ", ver, ", built: ", build.date, ")")
-  packageStartupMessage("Package rnrfa")
 
   # cat, for readability of the message text
 
   # RNRFA info - do not exceed 80char/line
   packageStartupMessage("
 +----------------------------------------------------------------+
-|  If you wish to use NRFA Data, please refer to the following   |
+|  If you wish to use NRFA data, please refer to the following   |
 |  Terms & Conditions:                                           |
 |  http://nrfa.ceh.ac.uk/costs-terms-and-conditions              |
 +----------------------------------------------------------------+
