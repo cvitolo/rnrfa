95af90aba3ca7676820d9dadd6ea6a2633501bda
Claudia Vitolo <claudia.vitolo@gmail.com>
2017-06-06 19:59:50 +0100

    Added default values to get_ts as suggested by @Xifip in issue #8

--- R/get_ts.R
+++ R/get_ts.R
@@ -24,7 +24,7 @@
 #' }
 #'
 
-get_ts <- function(id, type, metadata, cl, verbose){
+get_ts <- function(id, type, metadata = FALSE, cl, verbose = FALSE){
 
   options(warn=-1)                                       # do not print warnings
 
